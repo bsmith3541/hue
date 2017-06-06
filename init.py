49fbbb08dc1d3c1fc85e2121058920aa10aae8d1
Brandon Smith <anotherbrandonsmith@gmail.com>
2017-06-06 15:21:33 -0400

    Added initial implementation to get or user an existing username. Still need to add functionality to write a newly obtained username to a config.json file. For now, we'll store our private data in the config.json file that won't be included in our git repo.

--- init.py
+++ init.py
@@ -1,7 +1,9 @@
 import ssdp
 import requests
 import xml.etree.ElementTree as ET
-
+from urlparse import urlparse
+from pprint import pprint
+import json
 
 def findBridgeIP():
     devicesOnNetwork = ssdp.discover("IpBridge");
@@ -13,8 +15,24 @@ def findBridgeIP():
             root = ET.fromstring(response.text)
             for child in root:
                 if 'URLBase' in child.tag:
-                    bridgeIP = child.text
-            print "Bridge IP"
-            print bridgeIP
+                    bridgeHost = urlparse(child.text).hostname
+                    print "Bridge IP"
+                    print bridgeHost
+                    return bridgeHost
 
-findBridgeIP();
+def getOrFindUsername():
+    bridgeIP = findBridgeIP();
+    bridgeAPI = "http://{0}/api".format(bridgeIP)
+    with open('config.json') as data_file:
+        configData = json.load(data_file)
+        print "Config: Username"
+        if not configData["username"]:
+            payload = { "devicetype" : "FluxHue#Server" }
+            username = requests.post(bridgeAPI, json=payload)
+            print "--We just got a username--"
+            return username.text
+        else:
+            print "--We already have a username--"
+            return configData["username"]
+
+print getOrFindUsername()
